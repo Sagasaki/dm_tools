110fccf2127d5e7c6100470a27a5fd1157201cf6
Sagasaki <jaredchristopherallen@gmail.com>
2018-05-15 12:45:32 -0600

    Initial sync with github

---
+++ dice/dice.py
@@ -0,0 +1,25 @@
+##
+## Dice rolling tool
+## Copyright 2018 Jared C. Allen
+##
+
+"""
+This tool outputs the result of a dice roll based on inputs of the
+die type and any multipliers or modifiers (2d6, 1d4+3, etc).
+"""
+
+import random
+from colorama import Fore, Back, Style
+
+def roll_die():
+    die = int(input("Select die: ->d2, d4, d6, d8, d10, d12, d20, or d100 (# only): "))
+    if die not in [2, 4, 6, 8, 10, 12, 20, 100]:
+        print(Fore.RED + "Invalid die type. Please roll again.")
+        print(Style.RESET_ALL)
+        return
+    print(Fore.GREEN + "Rolling a d%i... " % (die), + random.randint(1, die))
+    print(Style.RESET_ALL)
+
+while True:
+    roll_die()
+
---
+++ dm_tools.py
@@ -0,0 +1,19 @@
+##
+## Dungeon Master's Digital Tool Set
+## Copyright 2018 Jared C. Allen
+##
+
+"""
+DM's Tools is a suite of useful tools to help a DM prepare for and run
+a Dungeons and Dragons (5e) session.
+
+This program should be able to:
+ ->Create and edit player character's complete character sheet
+ ->Manage encounters, including enemy HP, player HP, and track initiative
+ ->Easy dice rolls for any situation, including rolling each char. stat
+ ->Mapping tool for simple creation and tracking of dungeon adventures
+
+The dm_tools.py module is the MAIN file for the DM's Tools program.
+"""
+
+print("This program is working.")
