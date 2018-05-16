d829dc6e7a5ccaaa208b6990790f751d20e05e9a
Sagasaki <jaredchristopherallen@gmail.com>
2018-05-15 21:08:43 -0600

    combined print statments for colorama to remove extraneous newlines

--- dice/dice.py
+++ dice/dice.py
@@ -15,11 +15,9 @@ def roll_die():
     """Dice roll engine"""
     die = int(input("Select die: ->d2, d4, d6, d8, d10, d12, d20, or d100 (# only): "))
     if die not in [2, 4, 6, 8, 10, 12, 20, 100]:
-        print(Fore.RED + "Invalid die type. Please roll again.")
-        print(Style.RESET_ALL)
+        print(Fore.RED + "Invalid die type. Please roll again.", Style.RESET_ALL)
         return
-    print(Fore.GREEN + "Rolling a d%i... " % (die), + random.randint(1, die))
-    print(Style.RESET_ALL)
+    print(Fore.GREEN + "Rolling a d%i... " % (die), + random.randint(1, die), Style.RESET_ALL)
 
 while True:
     roll_die()
