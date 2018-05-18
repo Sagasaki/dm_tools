1e9e34f20997398b415cddbe2502d58e92b3a320
sagasaki <jaredchristopherallen@gmail.com>
2018-05-18 15:34:49 -0600

    Updated dice roller to allow +/- modifications to roll

--- dice/dice.py
+++ dice/dice.py
@@ -9,26 +9,34 @@ die type and any multipliers or modifiers (2d6, 1d4+3, etc).
 """
 
 import random
+import re
 from colorama import Fore, Style
-
-def roll_die():
-    """Dice roll engine"""
-    die = int(input("Select die: ->d2, d4, d6, d8, d10, d12, d20, or d100 (# only): "))
-    if die not in [2, 4, 6, 8, 10, 12, 20, 100]:
-        print(Fore.RED + "Invalid die type. Please roll again.", Style.RESET_ALL)
-        return
-    print(Fore.GREEN + "Rolling a d%i... " % (die), + random.randint(1, die), Style.RESET_ALL)
-
-#while True:
-#    roll_die()
 
 def die_roll():
     """Dice roll engine"""
-    die_input = input("Specify die roll in format '1d6': ").split('d')
+    die_input = input("Specify die roll in format '2d6': ").split("d")
     die = [int(value) for value in die_input]
-    result = str(random.randint(1, die[1]) * die[0])
-    print(Fore.GREEN + "Rolling %i d %i... " % (die[0], die[1]) + result, Style.RESET_ALL)
+    result = 0
+    for x in range (0, die[0]):
+        result = result + random.randint(1, die[1])
+    print(Fore.GREEN + "Rolling %i d %i... " % (die[0], die[1]) + str(result), Style.RESET_ALL)
+
+#while True:
+#        die_roll()
+
+def die_roll2():
+    """Dice roll engine"""
+    die_input = input("Specify die roll in format '2d6+3': ")
+    die = [int(x) for x in re.findall('\\d+', die_input)]
+    result = 0
+    for y in range (0, die[0]):
+        result = result + random.randint(1, die[1])
+    if '+' in die_input:
+        result = result + die[2]
+    elif '-' in die_input:
+        result = result - die[2]
+    print(Fore.GREEN + "Rolling %i d %i... " % (die[0], die[1]) + str(result), Style.RESET_ALL)
 
 while True:
-        die_roll()
+        die_roll2()
 
