a200d3e20a94a30c2afdc47de66e17db1428e0df
Sagasaki <jaredchristopherallen@gmail.com>
2018-05-25 13:18:02 -0600

    created new character manager module

--- char_man_dir/character.py
+++ char_man_dir/character.py
@@ -3,7 +3,7 @@
 """Doc string"""
 
 import pickle
-
+import os
 
 class Character:
     """Docstring"""
@@ -27,21 +27,87 @@ class Character:
         print(self.name + " the " + self.race + " " + self.cls)
 
 
-char_list = []
+def write_out(char_list):
+    with open("characters.dat", "wb+") as file:
+        pickle.dump(char_list, file)
 
-# read in previously created characters
-try:
-    with open("characters.dat", 'rb') as file:
-        char_list = pickle.load(file)
-except (FileNotFoundError):
-    pass
+def char_manager():
+    char_list = []
 
-char = Character.create()
-char_list.append(char)
+    # read in previously created characters
+    try:
+        with open("characters.dat", 'rb') as file:
+            char_list = pickle.load(file)
+    except (FileNotFoundError, EOFError):
+        pass
 
-for char in char_list:
-    char.summary()
+    print("What would you like to do?")
+    print("""Press q to exit.
+    1. List all characters
+    2. Create new character
+    3. Print detailed character information
+    4. Edit character
+    5. Delete character
+    """)
+    selection = input()
+    os.system('clear')
 
-# save character list to file
-with open("characters.dat", "wb+") as file:
-    pickle.dump(char_list, file)
+    if selection == '1':
+        print('\nComplete character list:')
+        for char in char_list:
+            char.summary()
+        print('\n')
+        char_manager()
+
+    elif selection == '2':
+        char = Character.create()
+        char_list.append(char)
+        write_out(char_list)
+        char_manager()
+
+    elif selection == '3':
+        name = input('\nType character name: ')
+        for char in char_list:
+            if char.name == name:
+                char.summary()
+            else:
+                print("No character with this name")
+        print('\n')
+        char_manager()
+
+    elif selection == '4':
+        name = input('Type character name: ')
+        for char in char_list:
+            if char.name == name:
+                char.update_class()
+                write_out(char_list)
+            else:
+                print("No character with this name")
+        char_manager()
+
+    elif selection =='5':
+        name = input('Type character name: ')
+        for char in char_list:
+            if char.name == name:
+                sel = input("Are you certain you wish to delete this character? Deletions are permanant. Type Y to continue.")
+                if sel == 'Y':
+                    char_list.remove(char)
+                    write_out(char_list)
+                    print("Character " + name + " has been deleted.")
+                else:
+                    print("Character not deleted.")
+            else:
+                print("No character with this name")
+        char_manager()
+
+
+    elif selection == 'q':
+        pass
+
+    else:
+        print("Invalid selection")
+        char_manager()
+
+    # save character list to file
+    with open("characters.dat", "wb+") as file:
+        pickle.dump(char_list, file)
