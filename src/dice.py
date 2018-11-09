4a132e7809ea7a0c5e1552d55fbf6171c9767dd0
sagasaki <jaredchristopherallen@gmail.com>
2018-11-09 08:49:23 -0700

    revised

--- src/dice.py
+++ src/dice.py
@@ -2,10 +2,11 @@
 ## Functions to allow for random die rolls ##
 
 import random
+import re
 
 
 class Dice():
-    def _roll(self, max):
+    def basic_roll(self, max):
         """ Engine that takes die type and returns the rolled value """
         result = random.randint(1, max)
         return result
@@ -13,7 +14,7 @@ class Dice():
         
     def custom_roll(self, multiple, die, modifier, modification):
         """ Allows for rolling die of the format 3d6+5 """
-        result = multiple * self._roll(die)
+        result = multiple * self.basic_roll(die)
         if modifier == '+':
             result = result + modification
         elif modifier == '-':
@@ -21,3 +22,36 @@ class Dice():
         else:
             pass
         return result
+
+        
+class DiceInterface(Dice):
+    def dice_prompt(self):
+        die_input = input("Specify die roll in format '2d6(+/-)3': ")
+        die = [int(x) for x in re.findall('\\d+', die_input)]
+        if die_input == 'q':
+            exit()
+        else:
+            try:
+                if len(die) == 1:
+                    print("Rolling a d%i... " % die[0] + self.basic_roll(die[0]))
+                    dice_prompt()
+                else:
+                    result = 0
+                    for y in range (0, die[0]):
+                        result = result + self.basic_roll(die[1])
+                    if '+' in die_input:
+                        result = result + die[2]
+                        print("Rolling %id%i +%i... " % (die[0], die[1], die[2]) + str(result))
+                    elif '-' in die_input:
+                        result = result - die[2]
+                        print("Rolling %id%i -%i... " % (die[0], die[1], die[2]) + str(result))
+                    else:
+                        print("Rolling %id%i... " % (die[0], die[1]) + str(result))
+            except IndexError:
+                print("Invalid input. Type q to exit.")
+                pass
+    
+
+    def interface(self):
+        while True:
+            self.dice_prompt()
